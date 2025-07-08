a901aecc0619ef28b70121df5247ca21fc630097
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-08 19:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function convertToRoman(num) {
 	if(num <= 0 || num > 100000){
 		return '';
 	}
-  	const obj = {
+  	const obj = [
      0:['M',1000], 
      1:['D', 500], 
      2:['C', 100], 
@@ -10,7 +10,7 @@ function convertToRoman(num) {
      4:['X', 10], 
      5:['V', 5], 
      6:['I', 1]
-	};
+	];
 
   //your code here
 let roman = '';
@@ -27,10 +27,10 @@ let roman = '';
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(100000));
+// console.log(convertToRoman(14));
 
 
 
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
